572d895c1336433cb1df4c472947955f93b0e691
karthikeyan_laptop <karthikeyanj1915@gmail.com>
2024-04-01 19:22:52 +0530

    added static files

--- backend/settings.py
+++ backend/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIRS = [os.path.join(BASE_DIR, "portfolio", "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(req):
-    return HttpResponse("Hi")
+    return render(req, "index.html")
