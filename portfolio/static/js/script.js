ba6f4a3f1c6fa829cf161b7b3e6efe3a3657aad1
karthikeyan_laptop <karthikeyanj1915@gmail.com>
2024-04-03 09:21:41 +0530

    minor changes and added projects modal

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -58,10 +58,21 @@ ScrollTrigger.create({
     navContainer.classList.add("nav-container");
   },
 });
-
 function toggleMenu() {
   var menu = document.getElementById("popup-menu");
-  menu.classList.toggle("show");
+  let popupAnimation = gsap.fromTo(
+    menu,
+    {
+      scale: 0,
+    },
+    {
+      scale: 1,
+      ease: "bounce.out",
+      duration: 0.3,
+      display: "block",
+    }
+  );
+  popupAnimation.play();
 }
 
 document.addEventListener("click", function (event) {
@@ -70,6 +81,17 @@ document.addEventListener("click", function (event) {
   var menuBtn = document.querySelector(".menu");
 
   if (!menu.contains(target) && target !== menuBtn) {
-    menu.classList.remove("show");
+    let popupAnimation = gsap.fromTo(
+      menu,
+      {
+        scale: 1,
+      },
+      {
+        scale: 0,
+        duration: 0.3,
+        display: "none",
+      }
+    );
+    popupAnimation.play();
   }
 });
